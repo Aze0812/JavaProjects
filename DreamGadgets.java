bd6fe52c7dc26b639c87ede8e83aa64bc769af3d
zjrncgit <140788153+Aze0812@users.noreply.github.com>
2025-07-22 15:08:02 +0800

    Add files via upload
    Simple Linkedlist example. Displays string values about DreamGadgets.

---
+++ DreamGadgets.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public class DreamGadgets
+{
+	public static void main(String[] args) {
+		LinkedList DreamGadgets = new LinkedList();
+		DreamGadgets.add("Ipad");
+		DreamGadgets.add("Laptop");
+		DreamGadgets.add("Personal Computer");
+		DreamGadgets.add("Iphone");
+		System.out.print(DreamGadgets);
+	}
+}
